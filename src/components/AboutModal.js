1a20a481306d8b5962158f1fbf753cfba00ff8fd
Marcia Moss <marcia_moss@hotmail.com>
2023-03-11 20:06:52 -0500

    modal css edits

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -22,7 +22,7 @@ const AboutModal = ({ showAbout, setShowAbout }) => {
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
+          <div className="fixed inset-0 overflow-y-auto bg-green-900">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
               <Transition.Child
                 as={Fragment}
@@ -33,7 +33,7 @@ const AboutModal = ({ showAbout, setShowAbout }) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-green-100 p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-serif font-medium leading-6 text-center"
@@ -57,7 +57,7 @@ const AboutModal = ({ showAbout, setShowAbout }) => {
                   <div className="mt-4 flex flex-col items-center">
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-green-300 border-2 border-black px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={() => setShowAbout(false)}
                     >
                       Got it, thanks!
