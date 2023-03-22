0a9a70688b36bd256a0bb7d3d071310692fe0671
Marcia Moss <marcia_moss@hotmail.com>
2023-03-21 21:54:43 -0400

    no data scenario

--- src/hooks/use-get-profile.js
+++ src/hooks/use-get-profile.js
@@ -7,10 +7,10 @@ const useGetProfile = ({ authUserId }) => {
     bio,
     profile_image_link = null;
   if (data) {
-    name = data[0].name;
-    location = data[0].location;
-    bio = data[0].bio;
-    profile_image_link = data[0].profile_image_link;
+    name = data[0]?.name;
+    location = data[0]?.location;
+    bio = data[0]?.bio;
+    profile_image_link = data[0]?.profile_image_link;
   }
   return [name, location, bio, profile_image_link, isFetching];
 };
