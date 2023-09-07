507ce45ba986bcc24187dc063265711c68f2bd57
Marcia Moss <marcia_moss@hotmail.com>
2023-09-06 23:15:51 -0400

    user profile fix

--- src/hooks/use-get-profile.js
+++ src/hooks/use-get-profile.js
@@ -6,9 +6,19 @@ const useGetProfile = ({ authUserId }) => {
   const dispatch = useDispatch();
   const { data } = useFetchUserQuery(authUserId);
   useEffect(() => {
-    if (data) {
+    if (data && data.length > 0) {
       const { name, location, bio, profile_image_link } = data[0];
 
+      dispatch(
+        userDataInfo({
+          user: { name, location, bio, profile_image_link },
+        })
+      );
+    } else {
+      let name,
+        location,
+        bio,
+        profile_image_link = null;
       dispatch(
         userDataInfo({
           user: { name, location, bio, profile_image_link },
